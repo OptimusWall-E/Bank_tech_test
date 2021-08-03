6019a8dcebbac6a443bc1d8bd59fdf5b67e9efa5
OptimusWall-E <iemailedmichael@gmail.com>
2021-08-03 17:25:59 +0100

    passing test for Date class, transaction_date returning dd/mm/yyyy

--- lib/date.rb
+++ lib/date.rb
@@ -1,3 +1,11 @@
 class Date
+  attr_reader :time
 
+  def initialize
+    @time = Time.new
+  end
+
+  def transaction_date
+    @time.strftime("%d/%m/%Y")
+  end
 end
--- spec/date_spec.rb
+++ spec/date_spec.rb
@@ -1,5 +1,11 @@
-require 'date_spec.rb'
+require 'date.rb'
 
 describe Date do
-  
+let(:time) {Time.new}
+  context '#today' do
+    it 'returns today\'s date' do
+      expect(subject.transaction_date).to eq (time.strftime("%d/%m/%Y"))
+    end
+  end
+
 end
