6b6d944952a5184acb2b61f32982573c7cd84afa
OptimusWall-E <iemailedmichael@gmail.com>
2021-08-03 16:54:26 +0100

    passing test for print_statement with both deposits and withdrawals

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -9,19 +9,30 @@ describe Statement do
       expect(subject.print_statement).to eq 'date || credit || debit || balance'
     end
 
-    it 'prints statement entry including credit' do
+    it 'prints statement entry including single credit' do
       ledger = Ledger.new
       statement = ledger.base_statement
       ledger.deposit(100.00)
       expect(statement.print_statement).to eq 'date || 100.00 || || 100.00'
     end
 
-    it 'prints statement entry including credit' do
+    it 'prints statement entry including single debit ' do
       ledger = Ledger.new
       statement = ledger.base_statement
       ledger.withdraw(100)
       expect(statement.print_statement).to eq 'date || || -100.00 || -100.00'
     end
+
+    it 'prints statement entry multiple debits and credits' do
+      ledger = Ledger.new
+      statement = ledger.base_statement
+      ledger.deposit(100.00)
+      ledger.deposit(100.00)
+      ledger.withdraw(50.00)
+      expect(statement.print_statement).to eq 'date || 100.00 || || 150.00'
+    end
+
+
   end
 
 end
